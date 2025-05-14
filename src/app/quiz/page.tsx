375e33feb6c37b8fcf856583e518e84d2f7e2cf7
victor-dv <js813417@gmail.com>
2025-05-14 19:37:36 -0300

    add

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,9 +1,37 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
-export default function quiz(){
+export default function Quiz(){
   return(
+    <View>
+      <View>
+        <Text>Adição</Text>
+        <Text>Selecione a melhor resposta para completar a continha</Text>
+      </View>
+    </View>
 
   )
 }
 
-const styles = StyleSheet.create
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  header:{
+    marginTop: 20,
+    paddingHorizontal: 50,
+    paddingVertical: 20,
+    alignItems:"center"
+  },
+  title:{
+    fontSize:24,
+    fontWeight:"bold",
+    color:"#000",
+    marginBottom:5
+  },
+  subtitle:{
+    fontSize: 16,
+    color:"#666",
+    textAlign:"center"
+  }
+})
